5f6ea1232fcfbfb0753d02910150295682679202
Shorya Pawar <22shorya23@gmail.com>
2023-06-13 13:49:07 +0530

    Completed Weather App

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+// export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6bcd5a59f4mshfa789c53d2efd59p1f50b7jsnce2036079247",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "03ce069173e328b3cc6a050f7830bf3f";
